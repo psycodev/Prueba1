c6dc2c1c7367e2e754fba87251326d7ee92274be
Camartinezm92 <Camartinezm92@gmail.com>
2022-08-18 22:43:59 -0500

    Operacion Multipliacar

---
+++ calculadora/calculadora.py
@@ -0,0 +1,21 @@
+
+def Multiplicar(a,b):
+    c = int(a*b)
+    w= print (" La multipliacion es igual a= ",c)
+    return (w)
+
+#a=int(input(" Ingrese el primer numero= "))
+#b=int(input(" Ingrese el segundo numero= "))
+Entradatexto= input("Ingrese la operaicon que desea realizar= ")
+Entradatexto= Entradatexto.lower()
+if Entradatexto=="mulplicar" or Entradatexto=="m":
+    if a!=0 and b!=0:
+        Multiplicar(a,b)
+    else:
+        "La Multipliacion es igual a 0"
+        c=0
+#else:
+ #   print("esta no esta")
+
+
+
