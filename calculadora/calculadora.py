a0aaadd61ff9d7357921a10dcf0946166099bcd9
carlosesuescun7506 <carlosesuescun1706@gmail.com>
2022-08-19 22:02:28 -0500

    adicion multiplicacion

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -4,37 +4,28 @@ print("2- Resta")
 print("3 - Multiplicacion")
 print("4 - Division")
 
+def Multiplicar(num1, num2):
+    c = int(num1*num2)
+    w= print (" La multipliacion es igual a= ",c)
+    return (w)
+
 while True:
     opcion = input("Selecciona una de las opciones: 1 - 2 - 3 - 4 ")
     if opcion in ('1', '2', '3', '4'):
         num1 = int(input("Ingresa el primer numero: "))
-        mun2 = int(input("ingresa el segundo numero: "))
+        num2 = int(input("ingresa el segundo numero: "))
         if opcion == '1':
             print(suma)
         elif opcion == '2':
             print(resta)
         elif opcion =='3':
-            multiplicar(a,b)
+             Multiplicar(num1,num2)
         elif opcion == '4':
             print(division)            
 
 
-def Multiplicar(a,b):
-    c = int(a*b)
-    w= print (" La multipliacion es igual a= ",c)
-    return (w)
 
-#a=int(input(" Ingrese el primer numero= "))
-#b=int(input(" Ingrese el segundo numero= "))
-Entradatexto= input("Ingrese la operaicon que desea realizar= ")
-Entradatexto= Entradatexto.lower()
-if Entradatexto=="mulplicar" or Entradatexto=="m":
-    if a!=0 and b!=0:
-        Multiplicar(a,b)
-    else:
-        "La Multipliacion es igual a 0"
-        c=0
-#else:
- #   print("esta no esta")
+
+
 
 
